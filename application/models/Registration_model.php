a39347ea3f5ddc9df46b7ad0e303e6c5d1ee09c4
BoppanaSandeep <boppanasandeep57@gmail.com>
2017-12-17 22:33:41 +0530

    First Commit

---
+++ application/models/Registration_model.php
@@ -0,0 +1,39 @@
+<?php
+class Registration_model extends CI_Model {
+
+    public function __construct()
+    {
+            $this->load->database();
+    }
+	
+	public function generte_rbid(){
+		$this->db->select("rb_id");
+		$this->db->order_by('rb_id',"desc");
+		$this->db->limit(1);
+		$rbid=$this->db->get("registration");
+		if(sizeof($rbid->result_array())>0){
+			foreach($rbid->result_array() as $rbid){
+				 $id=$rbid['rb_id'];
+			}
+			$rb_id=explode('RBFIR00000',$id);
+			$rb_id='RBFIR00000'.($rb_id[1]+1);
+			return $rb_id;
+		}else{
+			$rb_id='RBFIR000001';
+			return $rb_id;
+		}	
+	}
+	
+    public function InsertRegisterData($registerdata){
+
+        $reg=array(
+            "status"=>array()
+        );
+        $res=$this->db->insert("registration",$registerdata);
+        if($res){
+            return $reg['status']='OK';
+        }else{
+            return $reg['status']='BAD';
+        }
+    }
+}
