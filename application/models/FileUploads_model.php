0b37dc17f5e0f432f794ac19f67921700d72c0a6
BoppanaSandeep <boppanasandeep57@gmail.com>
2018-03-03 14:45:00 +0530

    Server name and Images.

--- application/models/FileUploads_model.php
+++ application/models/FileUploads_model.php
@@ -10,7 +10,7 @@ class FileUploads_model extends CI_Model
 
     public function profileUpload($user_id, $file_name)
     {
-        $data = array('profile_image' => $file_name);
+        $data = array('profile_image' => $file_name, 'updated_on'=> date('Y-m-d H:i:s'));
         $this->db->where('status', 1);
         $this->db->where('reg_id', $user_id);
         $res = $this->db->update('registration', $data);
